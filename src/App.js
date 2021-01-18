46b27d97df9f5872f8b7d900fce617b96f5368d8
kaen <kusdsuna@naver.com>
2021-01-18 17:57:18 +0900

    Add axios for App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -6,16 +8,33 @@ class App extends React.Component {
     movies: []
   };
 
+  getMovies = async () => { // async는 이 함수가 비동기라고 하는것
+    // API에서 데이타가져오기
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating'); // await는 뭘 기다리기 원할지
+    this.setState({ movies, isLoading: false });
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? 'Loading...' : movies.map(movie => (
+      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+    ))}</div>;
   }
 }
 
 export default App;
+
+// axios 설치
+// npm install axios
+
+// axios란?
+// fetch의 업그레이드 기능이 axios
+// 바닐라 자바스크립트로 구현된 비동기 통신 라이브러리
