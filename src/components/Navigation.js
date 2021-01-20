72a757e73bc9789ba3da5f76aab4330419aee169
kaen <kusdsuna@naver.com>
2021-01-21 00:34:41 +0900

    Fixed css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Navigation.css';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
-    return <div>
-        <Link to='/'>Home</Link>
+    return <nav>
+        {/* Link는 HTML에 href와 비슷하다고 생각하면 됨, to='링크'를 걸어주면됨 */}
+        <Link to='/'>Home</Link> 
         <Link to='/about'>About</Link>
-    </div>
+    </nav>
 }
 
 export default Navigation;
